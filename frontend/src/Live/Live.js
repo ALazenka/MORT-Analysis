63ce3ef80b46edef43ade0e8ecc6453fd4303aa4
ALazenka <andrewlazenka@gmail.com>
2018-02-03 16:00:57 -0500

    continuing to work on d3, changed page loading in app

--- frontend/src/Live/Live.js
+++ frontend/src/Live/Live.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
+import D3Chart from '../component/D3Chart/D3Chart'
 import './Live.css'
 
 class Live extends Component {
-    render() {
-        return(
-            <div className="Live">
-                
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div className="Live">
+        <D3Chart />
+      </div>
+    )
+  }
 }
 export default Live;

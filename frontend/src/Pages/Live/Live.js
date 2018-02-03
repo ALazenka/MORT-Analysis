141a3e62b79a0da7a218d361aa28a9d575ac54d6
ALazenka <andrewlazenka@gmail.com>
2018-02-03 18:27:55 -0500

    made some page changes

---
+++ frontend/src/Pages/Live/Live.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import './Live.css'
+
+class Live extends Component {
+  constructor() {
+    super();
+    this.state= {
+      startTime: Date.now(),
+      elapsed: 0
+    }
+  }
+
+  componentWillMount() {
+    const startTime = new Date()
+    this.setState({
+      startTime
+    })
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 100)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer)
+  }
+
+  tick() {
+    this.setState({
+      elapsed: Date.now() - this.state.startTime
+    })
+  }
+      
+  render() {
+    var elapsed = Math.round(this.state.elapsed / 100)
+    var display = (elapsed/10).toFixed(1)
+    return(
+      <div className="live">
+        <div className="top-row">
+          <div className="timer">
+            {Math.floor(display/60)}:{("0" + Math.round(display%60)).substr(-2)}
+          </div>
+          <div className="viewer-count">
+          </div>
+          <div className="timer-buttons">
+          </div>
+        </div>
+        <div className="live-analytics">
+          <div className="analytics-box-1"></div>
+          <div className="analytics-box-2"></div>
+          <div className="analytics-box-3"></div>
+          <div className="analytics-box-4"></div>
+          <div className="analytics-box-5"></div>
+          <div className="analytics-box-6"></div>
+          <div className="analytics-box-7"></div>
+          <div className="analytics-box-8"></div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Live;
